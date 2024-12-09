b933c6bcc037f9a2868d5e5a652a2d1619bde516
simera <olman4jesus@gmail.com>
2024-12-09 16:06:33 +0300

    refactor: migrate to React Router Framework declarative routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,48 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import {
+	RouterProvider,
+	createRoutesFromElements,
+	Route,
+	createBrowserRouter,
+	Navigate,
+} from "react-router-dom"
 import "./App.css"
 
 import AppLayout from "./pages/AppLayout"
-import { TasksProvider } from "./contexts/TasksContext"
+import TasksProvider from "./contexts/TasksContext"
 
 function App() {
-	const router = createBrowserRouter([
-		{
-			path: "/",
-			index: true,
-			element: <Navigate replace to="tasks" />,
-		},
-		{
-			path: "tasks",
-			element: <AppLayout />,
-			errorElement: <div>Error</div>,
-			children: [
-				{
-					path: "create", // Specific paths come before dynamic ones
-					element: <h1>Create New Task</h1>,
-				},
-				{
-					path: ":taskId", // Display task by ID
-					element: <h1>Display Task with ID</h1>,
-				},
-				{
-					path: ":taskId/edit", // Edit task
-					element: <h1>Edit Task with ID</h1>,
-				},
-				{
-					path: ":taskId/destroy", // Destroy task
-					element: <h1>Destroy Task with ID</h1>,
-				},
-			],
-		},
-	])
-
-	return (
-		<TasksProvider>
-			<RouterProvider router={router} />
-		</TasksProvider>
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route
+				path="/"
+				element={
+					<TasksProvider>
+						<AppLayout />
+					</TasksProvider>
+				}
+			>
+				<Route index element={<Navigate replace to="tasks" />} />
+				<Route path="tasks">
+					<Route path="create" element={<h1>Create New Task</h1>} />
+					<Route
+						path=":taskId"
+						element={<h1>Display Task with ID</h1>}
+					/>
+					<Route
+						path=":taskId/edit"
+						element={<h1>Edit Task with ID</h1>}
+					/>
+					<Route
+						path=":taskId/destroy"
+						element={<h1>Destroy Task with ID</h1>}
+					/>
+				</Route>
+				<Route path="*" element={<div>Error</div>} />
+			</Route>
+		)
 	)
+
+	return <RouterProvider router={router} />
 }
 
 export default App
